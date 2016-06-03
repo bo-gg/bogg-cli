9b17142a8aa45e487c6f404556f44ae7efde3764
Ben Friedland <ben174@gmail.com>
2016-06-02 23:41:34 -0700

    Working registration

--- cli.py
+++ cli.py
@@ -33,13 +33,121 @@ def cli(ate, calories, note):
 
 
 def setup():
-    pass
+    new_user()
 
 def log():
     pass
 
 def add_shortcut(calories, note):
     pass
+
+def new_user():
+    '''
+    {
+        "username": "ben16",
+        "email": "ben189@gmail.com",
+        "password": "balls",
+        "daily_weight_goal": 0.2,
+        "height": 72,
+        "weight": 270,
+        "activity_factor": 1.2,
+        "bogger": {
+            "gender": "M",
+            "birthdate": "1985-11-23",
+            "auto_update_goal": true
+        }
+    }
+    '''
+    username = click.prompt('Choose a username')
+    email = click.prompt('Enter your email')
+    while True:
+        password = click.prompt('Choose a password', hide_input=True)
+        confirm_password = click.prompt('Confirm password', hide_input=True)
+        if password == confirm_password:
+            break
+        click.echo('Passwords do not match. Try again.')
+    weekly_goal = click.prompt('How many pounds do you want to lose per week? (Usually between 1.0 and 2.0)',
+                               type=float)
+    daily_weight_goal = round(weekly_goal / 7.0, 3)
+    click.echo()
+    click.echo('The following questions are only used to calculate your basic metabolic rate and figure out ' \
+               'how many calories you should be eating per day.')
+    click.echo()
+    click.echo('Activity Level (https://en.wikipedia.org/wiki/Physical_activity_level)')
+    click.echo('  1. Sedentary')
+    click.echo('  2. Lightly Active')
+    click.echo('  3. Moderately Active')
+    click.echo('  4. Very Active')
+    click.echo('  5. Extra Active')
+    click.echo('Enter your activity level: ', nl=False)
+    while True:
+        c = click.getchar()
+        if c.isdigit():
+            break
+        click.echo('Invalid option. Enter 1-5.')
+    c = int(c)
+    activity_factor = 1.2
+    if c == 1:
+        activity_factor = 1.2
+    elif c == 2:
+        activity_factor = 1.375
+    elif c == 3:
+        activity_factor = 1.55
+    elif c == 4:
+        activity_factor = 1.725
+    elif c == 5:
+        activity_factor = 1.9
+
+    click.echo()
+    click.echo('Gender (m/f): ', nl=False)
+    while True:
+        gender = click.getchar().upper()
+        if gender in ('M', 'F'):
+            break
+        click.echo('Invalid option. Enter m or f.')
+    click.echo()
+
+    birthdate = click.prompt('Enter your birthdate (YYYY-MM-DD)')
+    height = click.prompt('Enter your height (in inches)', type=int)
+    weight = click.prompt('Enter your weight (in pounds)', type=int)
+
+    while True:
+        payload = {
+            'username': username,
+            'email': email,
+            'password': password,
+            'daily_weight_goal': daily_weight_goal,
+            'height': height,
+            'weight': weight,
+            'activity_factor': activity_factor,
+            'bogger': {
+                'gender': gender,
+                'birthdate': birthdate,
+                'auto_update_goal': True
+            }
+        }
+
+        response = requests.post(
+            'http://localhost:8000/api/create/',
+            json=payload,
+        )
+
+        try:
+            response.raise_for_status()
+            break
+        except:
+            data = response.json()
+            if 'username' in data.keys():
+                click.echo('Invalid username ({}): {}'.format(username, data['username'][0]))
+                username = click.prompt('Choose another username')
+            #TODO: handle other fields
+
+    data = response.json()
+    click.echo('User: {} created. Your daily calorie goal is: {}'.format(
+        data['username'],
+        data['bogger']['current_calorie_goal']
+    ))
+
 
 
 def create_measurement(weight):
@@ -167,5 +275,6 @@ def interactive():
     elif c == 7:
         show_log()
     elif c == 8:
+        setup()
         #TODO: Edit page for config
         pass
